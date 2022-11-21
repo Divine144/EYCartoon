e754f008c2c2e030f400b259f325e97478be943e
Divinity <69832329+Divine144@users.noreply.github.com>
2022-11-20 20:17:28 -0500

    [publish] fix elsa

--- src/main/java/com/nyfaria/eycartoon/client/renderer/ElsaRenderer.java
+++ src/main/java/com/nyfaria/eycartoon/client/renderer/ElsaRenderer.java
@@ -1,15 +1,26 @@
 package com.nyfaria.eycartoon.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.nyfaria.eycartoon.EYCartoon;
 import com.nyfaria.eycartoon.entity.ElsaEntity;
 import dev._100media.hundredmediageckolib.client.model.SimpleGeoEntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class ElsaRenderer extends GeoEntityRenderer<ElsaEntity> {
 
     public ElsaRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SimpleGeoEntityModel<>(EYCartoon.MODID, "elsa"));
+    }
+
+    @Override
+    public RenderType getRenderType(ElsaEntity animatable, float partialTick, PoseStack poseStack, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, int packedLight, ResourceLocation texture) {
+        return RenderType.entityCutoutNoCull(this.getTextureLocation(animatable));
     }
 
 }
